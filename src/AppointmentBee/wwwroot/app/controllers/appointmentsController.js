0d42b013c08d8dfd3c630f6d0a726ca47620a5d1
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2015-12-29 23:13:26 +0100

    Inital Commit

---
+++ src/AppointmentBee/wwwroot/app/controllers/appointmentsController.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+app.controller('appointmentsController', ['$scope', 'appointmentsService', function ($scope, appointmentsService) {
+
+    $scope.appointments = [];
+
+    appointmentsService.getAppointments().then(function (results) {
+
+        $scope.appointments = results.data;
+
+    }, function (error) {
+        alert(error.data.message);
+    });
+
+}]);
---
+++ src/AppointmentBee/wwwroot/app/services/appointmentsService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('appointmentsService', ['$http', function ($http) {
+
+    var serviceBase = 'http://calrest.azurewebsites.net/';
+    var appointmentsServiceFactory = {};
+
+    var _getAppointments = function () {
+
+        return $http.get(serviceBase + 'api/appointments').then(function (results) {
+            return results;
+        });
+    };
+
+    appointmentsServiceFactory.getAppointments = _getAppointments;
+
+    return appointmentsServiceFactory;
+
+}]);
