f7224c6cf81b0fadf0d2f8b92fa81960fc7bd322
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2016-05-03 21:05:13 +0200

    #5 First prototype of Patient Name suggestions.

---
+++ src/AppointmentBee/wwwroot/app/controllers/searchBoxController.js
@@ -0,0 +1,62 @@
+﻿'use strict';
+app.controller('SearchBoxController', ['$scope', '$location', '$timeout', 'patientsService', '$q', '$log',
+function ($scope, $location, $timeout, patientsService, $q, $log) {
+
+    $scope.simulateQuery = false;
+    $scope.isDisabled = false;
+    // array of patients
+    $scope.patients = "";
+    loadAll();
+    $scope.querySearch = querySearch;
+    $scope.selectedItemChange = selectedItemChange;
+    $scope.searchTextChange = searchTextChange;
+    $scope.newPatient = newPatient;
+    function newPatient(patient) {
+        alert("Create New Patient" + patient);
+    }
+
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for patients.
+     */
+    function querySearch(query) {
+        return results = query ? $scope.patients.filter(createFilterFor(query)) : $scope.patients,
+            deferred;
+    }
+    function searchTextChange(text) {
+        $log.info('Text changed to ' + text);
+    }
+    function selectedItemChange(item) {
+        $log.info('Item changed to ' + JSON.stringify(item));
+    }
+    /**
+     * Get all the patients at once. This is a cheap operation
+     */
+    function loadAll() {
+        
+        patientsService.getPatients().then(function (results) {
+
+            $scope.patients = results.data;
+
+        }, function (error) {
+            //alert(error.data.message);
+        });
+
+       
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+        return function filterFn(patient) {
+            return (patient.PatientName.indexOf(lowercaseQuery) === 0);
+        };
+
+    }
+}
+]);
+
+
---
+++ src/AppointmentBee/wwwroot/app/services/patientsService.js
@@ -0,0 +1,49 @@
+﻿'use strict';
+app.factory('patientsService', ['$http', 'serverSettings', function ( $http, serverSettings) {
+
+    var serviceBase = serverSettings.serviceBaseUri;
+
+    var patientsServiceFactory = {};
+
+    var _getPatients = function () {
+
+        return $http.get(serviceBase + 'api/patients').then(function (results) {
+            return results;
+        });
+    };
+
+    var _postPatient = function (data) {
+        
+        return $http.post(serviceBase + 'api/patients', data).then(function (results) {
+            return results;
+        });
+    };
+
+    var _deletePatient = function (data) {
+
+        return $http.delete(serviceBase + 'api/patients/' + data.Id, data).then(function (results) {
+            return results;
+        }, function (error) {
+            var tmp = error;
+            console.error(error.statusText);
+        });
+    };
+   
+    var _putPatient = function (data) {
+
+        return $http.put(serviceBase + 'api/patients/' + data.Id, data).then(function (results) {
+            return results;
+        }, function (error) {
+            var tmp = error;
+            console.error(error.statusText);
+        });
+    };
+
+    patientsServiceFactory.getPatients = _getPatients;
+    patientsServiceFactory.postPatient = _postPatient;
+    patientsServiceFactory.deletePatient = _deletePatient;
+    patientsServiceFactory.putPatient = _putPatient;
+
+    return patientsServiceFactory;
+
+}]);
