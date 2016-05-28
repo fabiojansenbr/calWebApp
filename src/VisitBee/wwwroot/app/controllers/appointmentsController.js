e736ecbf595afc9e159d792b3ad89906b869de54
Halil Karaca <halil.ibrahim.karaca@gmail.com>
2016-05-28 19:54:38 +0200

    Added post patient method for new patient dialog. Closes #40

--- src/VisitBee/wwwroot/app/controllers/appointmentsController.js
+++ src/VisitBee/wwwroot/app/controllers/appointmentsController.js
@@ -367,6 +367,11 @@ function ($scope, appointmentsService, calendarService, serverSettings, $mdDialo
         }
     };
 
+    $scope.DialogSavePatient = function () {
+        patientsService.postPatient($scope.newPatient).then(function (result) {
+            $mdDialog.hide();
+        })
+    };
 
     // *************************************************************************************
     // Smart search Box - Appointment dialog, Toolbar on top
@@ -380,11 +385,17 @@ function ($scope, appointmentsService, calendarService, serverSettings, $mdDialo
     $scope.querySearch = querySearch;
     $scope.selectedItemChange = selectedItemChange;
     $scope.searchTextChange = searchTextChange;
-    $scope.newPatient = newPatient;
+    $scope.createNewPatient = createNewPatient;
    
     //this function is used for md-autocomplete on topbar
-    function newPatient(patient) {
-        $scope.patient = patient;
+    function createNewPatient(patient) {
+
+        $scope.newPatient = {
+            patientName: patient,
+            phoneNumber: '',
+            note: ''
+        };
+
         var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
         $mdDialog.show({
             templateUrl: 'createNewClient',
